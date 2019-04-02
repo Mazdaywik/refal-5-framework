8734bbf726f5578aed6cd8d72eb07d2717286a00
Маздайщик <a.v.konovalov87@mail.ru>
2019-04-02 14:23:07 +0300

    Проект собрался и запустился на Рефале-05 (Mazdaywik/Refal-05#37, #3)

--- src/make-r05.bat
+++ src/make-r05.bat
@@ -3,6 +3,7 @@ setlocal
 call make-sr.bat
 set R05PATH=..\..\Refal-05\lib;..\..\Refal-05\src
 call ..\..\Refal-05\c-plus-plus.conf.bat
+if exist main-r05.exe erase main-r05.exe
 ..\..\Refal-05\bin\refal05c ^
   out\Main ^
   out\Lexer ^
@@ -14,4 +15,8 @@ call ..\..\Refal-05\c-plus-plus.conf.bat
   Library ^
   refal05rts
 erase *.obj *.tds
+ren main.exe main-r05.exe
+main-r05.exe _tests_
+main-r05.exe Main.ref out/Main.r05.ref
+main-r05.exe Parser.ref out/Parser.r05.ref
 endlocal
