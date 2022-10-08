873b052118b6ad242e88f89c37d6b073750ac300
Маздайщик <a.v.konovalov87@mail.ru>
2022-10-09 00:38:46 +0300

    Уточнение скриптов тестирования

--- tests/parser/run.cmd
+++ tests/parser/run.cmd
@@ -19,6 +19,7 @@ goto :EOF
 :RUN_TEST
 setlocal
   echo Parsing %1...
+  set REF5RSL=..\..\lib
   echo Y| refgo test-parser+R5FW-Parser+R5FW-Parser-Defs+LibraryEx %1 2>__err.txt
   if errorlevel 1 (
     echo Parser failed, see __err.txt for details
